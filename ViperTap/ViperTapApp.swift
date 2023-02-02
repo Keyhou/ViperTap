0be7edd4eab1fc18a4c3812d71d5087ade4ad250
Keyhou <31210145+Keyhou@users.noreply.github.com>
2023-02-02 14:57:56 +0100

    Initial Commit

---
+++ ViperTap/ViperTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViperTapApp.swift
+//  ViperTap
+//
+//  Created by Keyhou on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViperTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
